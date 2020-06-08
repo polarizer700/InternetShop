3c22f97763c5660c01d2db14acfe71920cdf8a0a
Vladimir <polarizer700@gmail.com>
2020-06-08 12:39:26 +0300

    Главный экран

---
+++ script.js
@@ -0,0 +1,35 @@
+firstList.onmouseover = function(){
+	firstA.style.borderBottom = "none";
+  firstA.style.color = "#000000";
+}
+firstList.onmouseout = function(){
+	firstA.style.borderBottom = "1px dashed #ff5816";
+  firstA.style.color = "#ff5816";
+}
+
+firstList2.onmouseover = function(){
+	firstA2.style.borderBottom = "none";
+  firstA2.style.color = "#000000";
+}
+firstList2.onmouseout = function(){
+	firstA2.style.borderBottom = "1px dashed #ff5816";
+  firstA2.style.color = "#ff5816";
+}
+
+firstList3.onmouseover = function(){
+	firstA3.style.borderBottom = "none";
+  firstA3.style.color = "#000000";
+}
+firstList3.onmouseout = function(){
+	firstA3.style.borderBottom = "1px dashed #ff5816";
+  firstA3.style.color = "#ff5816";
+}
+
+firstList4.onmouseover = function(){
+	firstA4.style.borderBottom = "none";
+  firstA4.style.color = "#000000";
+}
+firstList4.onmouseout = function(){
+	firstA4.style.borderBottom = "1px dashed #ff5816";
+  firstA4.style.color = "#ff5816";
+}
